3af8fdfbc2fc94bf2a6f7de7500e201de7297b57
Daisy Jepleting <jepledaisy@gmail.com>
2022-06-13 23:44:57 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+function callBack(){
+    document.getElementById("text").textContent = "This is really cool!"}
+
+document.addEventListener('DOMContentLoaded',function(){
+    console.log('The DOM has loaded');
+    console.log(callBack());
+
+});
+
